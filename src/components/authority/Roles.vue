dbb3f244cd1e3b9a935a912b6b711dbae46170e7
lvhang <15523352161@163.com>
2020-10-23 11:12:23 +0800

    获取用户列表的数据

--- src/components/authority/Roles.vue
+++ src/components/authority/Roles.vue
@@ -1,11 +1,51 @@
 <template>
   <div>
-    <h3>角色列表页面</h3>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 面包屑导航区域 -->
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 添加角色按钮区域 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+      <!-- 添加角色按钮区域 -->
+      <!-- 角色列表区域 -->
+      <!-- 角色列表区域 -->
+    </el-card>
+    <!-- 卡片视图区域 -->
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      //   所有角色列表的数据
+      rolesList: []
+    }
+  },
+  created () {
+    this.getRolesList()
+  },
+  methods: {
+    // 获取所有角色的列表！
+    async getRolesList () {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败！')
+      }
+      this.rolesList = res.data
+      console.log(this.rolesList)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
